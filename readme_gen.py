a2ced7ede01e9f399bd7241569e326e3a7d3461e
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-09-28 11:58:52 +0300

    Правка генератора описания проекта

--- readme_gen.py
+++ readme_gen.py
@@ -9,13 +9,17 @@ def parse_project_structure(path=Path("."), indent=0, include_files=True, is_las
     structure = ""
 
     items = list(path.iterdir())
+    if include_files:
+        items = sorted(items, key=lambda x: (x.is_file(), x))
+        EXCLUDED_FOLDERS.extend(excluded_files)
+        items = [item for item in items if item.name not in EXCLUDED_FOLDERS]
 
     for i, item in enumerate(items):
         is_last_item = (i == len(items) - 1)
 
-        if item.is_file() and include_files and item.name not in excluded_files:
+        if item.is_file():
             structure += f"{'│   ' * indent}{'└── ' if is_last_item else '├── '}{item.name}\n"
-        elif item.is_dir() and item.name not in EXCLUDED_FOLDERS:
+        elif item.is_dir():
             folder_name = item.name
             structure += f"{'│   ' * indent}{'└── ' if is_last_item else '├── '}{folder_name}"
 
@@ -36,13 +40,16 @@ def parse_project_structure(path=Path("."), indent=0, include_files=True, is_las
 
 
 # Пример использования:
-project_path = Path("/Users/vmenshikov/PycharmProjects/dm_api_tests")  # Укажите путь к вашему проекту
+project_path = Path("/Users/vmenshikov/PycharmProjects/pvz-qa-api")  # Укажите путь к вашему проекту
 include_files = True  # Установите значение False, чтобы отображать только папки
-excluded_files = [".gitignore", "__init__.py"]  # Укажите файлы, которые нужно исключить
+excluded_files = [
+    ".gitignore",
+    "__init__.py"
+]  # Укажите файлы, которые нужно исключить
 folder_descriptions = {
-    "generic": "Описание папки 1",
-    "data": "Описание папки 1",
-    "helpers": "Описание папки 1",
+    "routes": "Описание папки 1",
+    "utilities": "Описание папки 1",
+    "clients": "Описание папки 1",
 }
 project_structure = parse_project_structure(
     path=project_path,
